a1786a5aee49befb0d1ecd3f7a903eed3248aaf9
thfr <thfr.git@e.mail.de>
2019-12-23 14:42:02 +0100

    Zooming with "+" and "-" key, moving with arrow keys

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_scancode.h>
 #include <SDL2/SDL_video.h>
 
 #include <cassert>
@@ -29,9 +31,14 @@ int main()
     const unsigned int texWidth  = 1024;
     const unsigned int texHeight = 768;
     const size_t maxIterations   = 1024;
+    const double realRange       = 3.0;
+    const double imagRange       = (texWidth > texHeight) ? realRange * texHeight / texWidth
+                                                    : realRange * texHeight / texWidth;
 
     Mandelbrot<double> mb;
-    ComplexView<double> mbView{{-0.5, 0.0}, 3.0, 2.25, texWidth, texHeight, maxIterations};
+    ComplexView<double> mbViewDefault{{-0.5, 0.0}, realRange, imagRange,
+                                      texWidth,    texHeight, maxIterations};
+    ComplexView<double> mbView = mbViewDefault;
 
     auto window = SDL_CreateWindow("Almondbread", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                                    texWidth, texHeight, SDL_WINDOW_SHOWN);
@@ -55,6 +62,50 @@ int main()
             if ((SDL_QUIT == event.type) ||
                 (SDL_KEYDOWN == event.type && SDL_SCANCODE_ESCAPE == event.key.keysym.scancode)) {
                 running = false;
+                break;
+            }
+            if (!(SDL_KEYDOWN == event.type)) {
+                continue;
+            }
+            switch (event.key.keysym.sym) {
+            // ZOOM out:
+            case SDLK_MINUS:
+            case SDLK_KP_MINUS:
+                mbView.imagRange = 1.0 / 0.75 * mbView.imagRange;
+                mbView.realRange = 1.0 / 0.75 * mbView.realRange;
+                break;
+            // ZOOM in:
+            case SDLK_PLUS:
+            case SDLK_KP_PLUS:
+                mbView.imagRange = 0.75 * mbView.imagRange;
+                mbView.realRange = 0.75 * mbView.realRange;
+                break;
+            // move up:
+            case SDLK_UP:
+                mbView.center += complex<double>{0, 0.25 * mbView.imagRange};
+                break;
+            // move down:
+            case SDLK_DOWN:
+                mbView.center += complex<double>{0, -0.25 * mbView.imagRange};
+                break;
+            // move left:
+            case SDLK_LEFT:
+                mbView.center += complex<double>{-0.25 * mbView.realRange, 0};
+                break;
+            // move right:
+            case SDLK_RIGHT:
+                mbView.center += complex<double>{0.25 * mbView.realRange, 0};
+                break;
+            case SDLK_0:
+                mbView.center    = mbViewDefault.center;
+                mbView.realRange = mbViewDefault.realRange;
+                mbView.imagRange = mbViewDefault.imagRange;
+                break;
+            // quit:
+            case SDLK_q:
+                running = false;
+                break;
+            default:
                 break;
             }
         }
